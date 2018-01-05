7f072316cd4ad88a53bf4cf56d3ecc19514d4d22
Vlad Riscutia <vladris@outlook.com>
2018-01-04 16:48:20 -0800

    Add project files.

---
+++ docx2txt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace docx2txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
